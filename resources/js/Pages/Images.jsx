aa742e64a1c4197aa5ac7fc85a7a071133c70d72
Fabien Lapert <flapert@feelagile.com>
2023-06-03 15:27:12 +0200

    update home page

--- resources/js/Pages/Images.jsx
+++ resources/js/Pages/Images.jsx
@@ -11,7 +11,7 @@ export default function Images({ auth }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                        <div className="p-6 text-gray-900">Mes Images</div>
                     </div>
                 </div>
             </div>
