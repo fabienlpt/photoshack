8abf986ee064e5cc4db50c13d51d88397877d937
Fabien Lapert <flapert@feelagile.com>
2023-06-04 21:29:16 +0200

    rename home page

--- resources/js/Pages/Accueil.jsx
+++ resources/js/Pages/Accueil.jsx
@@ -16,19 +16,21 @@ export default function Dashboard({ auth }) {
         <AuthenticatedLayout
             user={auth.user}
         >
-            <Head title="Dashboard" />
+            <Head title="Accueil" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">Accueil</div>
-                        {
-                            images.map((image) => (
-                                <div className="p-6 bg-white border-b border-gray-200">
-                                    <img src={image.path} alt={image.name} width={300} onClick={() => window.open(image.url, '_blank')} />
-                                </div>
-                            ))
-                        }
+                        <div className='flex flex-wrap w-full justify-between'>
+                            {
+                                images.map((image) => (
+                                    <div className="bg-white p-auto">
+                                        <img src={image.path} alt={image.name} width={400} onClick={() => window.open(image.url, '_blank')} />
+                                    </div>
+                                ))
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
